0e6ce14621b07f56d90a67143a2972140b54e013
Nefalit <nefalit7@gmail.com>
2022-05-26 19:45:26 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  setTime('videoplayer-current-time', data.seconds);
+}
+
+
+function setTime(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getTime(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+player.setCurrentTime(getTime('videoplayer-current-time'));
